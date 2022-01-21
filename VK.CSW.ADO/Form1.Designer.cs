edd7242746337aeea4fcf0aa8ec87ae424a4b0b6
vadim khvostov <vkhvostov@gmail.com>
2022-01-21 11:25:21 +0300

    ADO added

---
+++ VK.CSW.ADO/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace VK.CSW.ADO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvResult = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvResult
+            // 
+            this.dgvResult.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvResult.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvResult.ColumnHeadersHeight = 29;
+            this.dgvResult.Location = new System.Drawing.Point(0, 0);
+            this.dgvResult.Name = "dgvResult";
+            this.dgvResult.RowHeadersWidth = 51;
+            this.dgvResult.Size = new System.Drawing.Size(808, 450);
+            this.dgvResult.TabIndex = 0;
+            this.dgvResult.Layout += new System.Windows.Forms.LayoutEventHandler(this.dgvResult_Layout);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvResult;
+    }
+}
+
---
+++ VK.CSW.ADO/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace VK.CSW.ADO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void dgvResult_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=ExamDB;Data Source=LAPTOP-DHBMFRC4\\SQLEXPRESS"))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT  nPokazatelId, vPeriodType, dtStartDate, dtEndDate, vTerritoryId, nValue FROM[ExamDB].[Istochnik].[TableSource]", conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        dgvResult.Columns.Add("nPId", "nPId");
+                        dgvResult.Columns.Add("pType", "pType");
+                        dgvResult.Columns.Add("StartDate", "StartDate");
+                        dgvResult.Columns.Add("EndDate", "EndDate");
+                        dgvResult.Columns.Add("TId", "TId");
+                        dgvResult.Columns.Add("nValue", "nValue");
+
+                        while (reader.Read())
+                        {
+                            int idx = dgvResult.Rows.Add();
+                            for(int i = 0; i < reader.FieldCount; ++i)
+                            {
+                                dgvResult.Rows[idx].Cells[i].Value = reader[i].ToString();
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
